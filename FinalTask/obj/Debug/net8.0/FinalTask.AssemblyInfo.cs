388c031539e943b81153a1d169cb33dd77f31932
oleg voronin <oleg83voronin@gmail.com>
2024-06-26 15:46:07 +0300

    add files

--- FinalTask/obj/Debug/net8.0/FinalTask.AssemblyInfo.cs
+++ FinalTask/obj/Debug/net8.0/FinalTask.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinalTask")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89ed3487ba60591438c17018883386ef0698ec76")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64274cee2c65ddc277430f9740f08b9319508b63")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinalTask")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinalTask")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
